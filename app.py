9850d4da1f97a803c056f22027a31a4a95de6b36
dperevoz <dmisep@gmail.com>
2025-08-01 22:36:35 +0300

    App: car age and mileage charts

--- app.py
+++ app.py
@@ -65,17 +65,26 @@ df_filtered_pr_year = df_filtered_pr[df_filtered_pr['model_year'] >= 1960]
 
 
 ### Streamlit app ###
-# Setup
+# Setup and intro
 st.set_page_config(layout="wide")
 st.title('Car sales dashboard')
 st.markdown('''
          **This app allows to explore which factors impact prices for used cars**
          Below you can see:
-         - A **dashboard** allowing to explore how different categorical characteristics impact the price.
-         - A **histogram** showing correlation between price and vehicle's age.
-         - A **scatterplot** showing correlation between price and vehicle's mileage.
+         1. A **dashboard** allowing to explore how different categorical characteristics impact the price.
+         2. A **histogram** showing correlation between price and vehicle's age.
+         3. A **scatterplot** showing correlation between price and vehicle's mileage.
+        
+         By default, extreme outliers by price (top %1) are excluded. To see the whole price range, please uncheck the checkbox below.
          ''')
 
+# Checkbox to exclude price outliers from the plot
+exclude_outliers = st.checkbox(
+    'Exclude outliers (top %1 by price)',
+    value=True,
+    key='exclude_price_outliers'
+)
+chosen_df = df_filtered_pr if exclude_outliers else df
 
 
 
@@ -83,6 +92,7 @@ st.markdown('''
 # Dashboard: box plot of price distributions by categorical characteristics
 #   Allows to compare price distributions by a chosen parameter
 #   Allows to exclude outliers based on a checkbox selection
+
 st.header('1. Price distribution by different vehicle characteristics')
 st.markdown('''
             Using this dashboard you can explore how the following characteristics impact vehicle's price
@@ -94,12 +104,12 @@ st.markdown('''
             - transmission type,
             - drivetrain type (4WD vs AWD),
             - number of cylinders.
-            
+            \n\n
             Each category is presented as a box plot:
             - The boxes represent the price range for middle %50 of all cars in the category.
             - Horizontal lines inside each box represent median price for the category.
             - Dashed horizontal line on the chart represents overall median price for the whole database. 
-
+            \n\n
             Outliers by price (top %1) are excluded by default. To see the whole range, please uncheck the checkbox below.
     ''')
 
@@ -121,13 +131,6 @@ param_to_compare = st.selectbox(
 def conditions_order(df, param):
     return df.groupby(param)['price'].median().sort_values().index.tolist()
 
-# Checkbox to exclude outliers from the plot
-exclude_outliers = st.checkbox(
-    'Exclude outliers (top %1 by price)',
-    value=True,
-    key='exclude_price_outliers'
-)
-chosen_df = df_filtered_pr if exclude_outliers else df
 
 # Creating the box plot
 fig = px.box(
@@ -139,20 +142,32 @@ fig = px.box(
         title=f'Price distribution by vehicle\'s {str(param_to_compare)}',
         height=600,
     )
+fig.add_hline(y=chosen_df['price'].median(), line_dash='dash',
+              annotation_text='Median', annotation_position='top right',
+              line_color='green')
 st.write(fig)
+
+
+
 
 
 
 # Second chart: histogram of price distribution by 'model_year'
 #   Allows to filter by vehicle's condition
 
-st.header('2. Price distribution by year of manufacture')
+st.header('2. Price distribution by vehicle\'s age')
 st.markdown('''
-    This chart shows the distribution of prices by year of manufacture.
+            This histogram shows the median car prices by year of manufacture.
+            Cars older tham 1960 are exluded to remove outliers (0-2 cars per year)
+            \n\n
+            To filter by vehicle's condition, please use the selector below.
+            
     ''')
 
+
+
 # Filtering out rows with 'model_year' less than 1960 to exclude outliers (0 or 1 car per year)
-df_by_year = df_filtered[df_filtered['model_year'] > 1960]
+df_filtered_year = chosen_df[chosen_df['model_year'] > 1960]
 
 # Creating a multiselect box for filtering by vehicle's condition
 # The conditions are sorted from worst to best
@@ -163,11 +178,11 @@ st.multiselect(
     default=conditions,
     key='condition_filter'
 )
-df_by_year = df_by_year[df_by_year['condition'].isin(st.session_state.condition_filter)]
+df_filtered_year = df_filtered_year[df_filtered_year['condition'].isin(st.session_state.condition_filter)]
 
 # Creating the histogram
 fig = px.histogram(
-        df_by_year,
+        df_filtered_year,
         y='price', 
         x='model_year', 
         histfunc='avg',
@@ -176,4 +191,34 @@ fig = px.histogram(
     )
 st.write(fig)
 
+
+
+
+
+# Third chart: scatterplot of price distribution by 'odometer'
+
+st.header('3. Price distribution by vehicle\'s mileage (odometer reading)')
+st.markdown('''
+            This scatterplot shows distribution of car prices by odometer reading at the time of sale.
+            Extreme outliers by mileage (top %1) are excluded.
+            ''')
+
+# Creating the scatterplot
+# Price distribution by 'odometer'
+
+# Calculate 99th percentile of odometer
+odometer_cutoff = chosen_df['odometer'].quantile(0.99)
+
+# Filter the DataFrame
+df_filtered_odometer = chosen_df[chosen_df['odometer'] <= odometer_cutoff]
+
+fig = px.scatter(
+    df_filtered_odometer, 
+    x='odometer', 
+    y='price', 
+    opacity=0.2,
+    trendline='ols',
+    title="Price distribution by odometer", 
+    height=600)
+st.write(fig)
 
